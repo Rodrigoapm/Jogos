50c95f681b620da4701366a14b10e7d9ea143f14
1701465 <www.rodasapm@hotmail.com>
2023-05-05 09:52:51 +0100

    cria classe que representa a tabela categorias

--- app/src/main/java/com/example/livros/TabelaBD.kt
+++ app/src/main/java/com/example/livros/TabelaBD.kt
@@ -3,6 +3,7 @@ package com.example.livros
 import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
 abstract class TabelaBD (val db: SQLiteDatabase?, val nome: String) {
     abstract fun cria()
@@ -27,5 +28,9 @@ abstract class TabelaBD (val db: SQLiteDatabase?, val nome: String) {
 
     fun elimina( where: String, argsWhere: Array<String>) =
         db.delete(nome, where, argsWhere)
+
+    companion object {
+        const val CHAVE_TABELA = "${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT"
+    }
 
     }
